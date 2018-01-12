44fe3173495fc62a94867bcaf6b5a81cbd504e6a
it's mercredi my dudes AAAAAAAA <ankadre@hotmail.fr>
2018-01-12 11:59:54 +0100

    i've done almost nothing lmao (12/01/2018)

--- src/raytracing.c
+++ src/raytracing.c
@@ -71,7 +71,7 @@ t_rgb		getlight(t_vec *norm, t_light **light, t_rgb *colorobj, t_stuff *e)
 		rgb.r += (*light)->color.r * angle * (*light)->diff;
 		rgb.g += (*light)->color.g * angle * (*light)->diff;
 		rgb.b += (*light)->color.b * angle * (*light)->diff;
-		getspeclight(e, norm, &rgb, light);
+		//getspeclight(e, norm, &rgb, light);
 		return (rgb);
 	}
 	if ((*light)->nm == 0)
@@ -81,24 +81,21 @@ t_rgb		getlight(t_vec *norm, t_light **light, t_rgb *colorobj, t_stuff *e)
 
 int		raythingy(t_stuff *e)
 {
-	double dist;
-
 	check(e, &e->raydir, &e->poscam, 1);
 	check_dist(e, 1);
-	dist = e->c.dist;
 	reboot_list_loop(e, 3);
 	e->c.colorf.r = 0;
 	e->c.colorf.g = 0;
 	e->c.colorf.b = 0;
 	if (e->c.obj >= 0 && e->c.obj <= 3)
 	{
-		getintersection(e, dist);
+		getintersection(e, e->c.dist);
 		while (e->light)
 		{
 			getlightdir(e, e->c.inter);
 			if (e->c.obj == SPHERE)
 			{
-				searchlist(e, e->c.objsph, e->c.obj);
+				searchlist(e, e->c.objsph, SPHERE);
 				vecsous(&e->sph->norm, &e->c.inter, &e->sph->pos);
 				vecnorm(&e->sph->norm);
 				rgb_add(&e->c.colorf, e->c.colorf, \
@@ -106,13 +103,13 @@ int		raythingy(t_stuff *e)
 			}
 			else if (e->c.obj == PLAN)
 			{
-				searchlist(e, e->c.objpla, e->c.obj);
+				searchlist(e, e->c.objpla, PLAN);
 				rgb_add(&e->c.colorf, e->c.colorf, \
 					getlight(&e->pla->norm, &e->light, &e->pla->color, e), 1);
 			}
 			else if (e->c.obj == CYLINDRE)
 			{
-				searchlist(e, e->c.objcyl, e->c.obj);
+				searchlist(e, e->c.objcyl, CYLINDRE);
 				vecsous(&e->cyl->norml, &e->c.inter, &e->cyl->pos);
 				vecnorm(&e->cyl->norml);
 				rgb_add(&e->c.colorf, e->c.colorf, \
@@ -120,7 +117,7 @@ int		raythingy(t_stuff *e)
 			}
 			else if (e->c.obj == CONE)
 			{
-				searchlist(e, e->c.objcone, e->c.obj);
+				searchlist(e, e->c.objcone, CONE);
 				vecsous(&e->cone->norml, &e->c.inter, &e->cone->pos);
 				vecnorm(&e->cone->norml);
 				rgb_add(&e->c.colorf, e->c.colorf,\
@@ -132,11 +129,9 @@ int		raythingy(t_stuff *e)
 		if (shadows(e, &e->c.inter) == 1)
 			return (0);
 	}
-	else if (e->c.obj == 4)
+	else if (e->c.obj == LIGHT)
 	{
-		searchlist(e, e->c.objlight, e->c.obj);
-		vecsous(&e->light->norm, &e->c.inter, &e->light->pos);
-		vecnorm(&e->light->norm);
+		searchlist(e, e->c.objlight, LIGHT);
 		rgb_add(&e->c.colorf, e->c.colorf, e->light->color, e->light->diff);
 	}
 	return (0);
@@ -185,12 +180,12 @@ void		aff(t_stuff *e)
 
 void		check(t_stuff *e, t_vec *raydir, t_vec *pos, int option)
 {
-	e->c.dist = 9999;
-	e->c.distsph = 9999;
-	e->c.distpla = 9999;
-	e->c.distcyl = 9999;
-	e->c.distcone = 9999;
-	e->c.distlight = 9999;
+	e->c.dist = INFINITY;
+	e->c.distsph = INFINITY;
+	e->c.distpla = INFINITY;
+	e->c.distcyl = INFINITY;
+	e->c.distcone = INFINITY;
+	e->c.distlight = INFINITY;
 	e->c.obj = -1;
 	while (e->sph)
 	{
